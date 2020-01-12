507903651c0af3d5e62a377984b93b77c4bbbb7a
sudhamsuanil <55593152+sudhamsuanil@users.noreply.github.com>
2020-01-12 18:13:43 +0530

    first azure commit

---
+++ firstPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace firstPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
